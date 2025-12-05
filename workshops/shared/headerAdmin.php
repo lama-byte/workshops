1ec61048d022ec2c257ceea5ca3ad478b6c97b89
Ahad Alanazi <ahadfarah10@gmail.com>
2025-12-05 07:16:06 +0300

    Add headerAdmin.php for admin header layout

--- workshops/shared/headerAdmin.php
+++ workshops/shared/headerAdmin.php
@@ -1,1 +1,18 @@
-
+<?php
+echo '
+    <header>
+        <section>
+            <h1 class="logo">Learning Aura</h1>
+        </section>
+        <nav class="headerNav">
+            <div id="div_left">
+                <a href="/workshops/workshops/public/index.php">Home</a>
+                <a href="#contactInfo">Contact</a>
+            </div>
+            <div id="div_right">
+                <a href="/workshops/workshops/public/signout.php">Sign out</a>
+            </div>
+        </nav>
+    </header>
+';
+?>
